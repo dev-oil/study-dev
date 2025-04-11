5794b037ce004ed6361e840190e05e839dfbe1e1
devoil <devdevoildev@gmail.com>
2025-04-11 17:37:58 +0900

    Feat: [html5] location과 weather

---
+++ html5/location-weather/weather.ts
@@ -0,0 +1,66 @@
+{
+  const resultEl: HTMLElement | null = document.getElementById('result')!;
+  const weatherElement: HTMLElement | null =
+    document.getElementById('weather')!;
+
+  (window as any).myPosition = function (): void {
+    if (!navigator.geolocation) {
+      resultEl.textContent = '이 브라우저에서는 위치 정보를 지원하지 않습니다!';
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(currentPosition, errorCallback);
+  };
+
+  const getWeather = (lat: number, lng: number) => {
+    var apiKey = '';
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${apiKey}`;
+
+    fetch(apiUrl)
+      .then(function (res) {
+        console.log(res);
+        return res.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        var weatherdes = data.weather[0].description;
+        var temperature = data.main.temp;
+        var cityName = data.name;
+
+        var weatherInfo = `도시: ${cityName}<br>
+          날씨: ${weatherdes}<br>
+          온도: ${temperature}<br>
+          `;
+
+        weatherElement.innerHTML = weatherInfo;
+      })
+      .catch(function (error) {
+        console.error('날씨 정보를 가져오는 중 오류 발생: ', error);
+      });
+  };
+
+  // 현재 위치 값 구하기
+  const currentPosition = (position: GeolocationPosition) => {
+    const { latitude, longitude } = position.coords;
+    resultEl!.textContent = `📍 현재 위치: 위도 ${latitude}, 경도 ${longitude}`;
+
+    getWeather(latitude, longitude);
+
+    weatherElement.innerHTML = '';
+  };
+
+  // 에러 처리
+  const errorCallback = (error: GeolocationPositionError) => {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        resultEl.textContent = `❌ 현재 위치 정보에 대한 권한이 없습니다.: ${error.message}`;
+        break;
+      case error.POSITION_UNAVAILABLE:
+        resultEl.textContent = `❌ 현재 위치 정보를 구할 수 없습니다.: ${error.message}`;
+        break;
+      case error.TIMEOUT:
+        resultEl.textContent = `❌ 시간 제한을 초과했습니다.: ${error.message}`;
+        break;
+    }
+  };
+}
---
+++ html5/location/location.ts
@@ -0,0 +1,69 @@
+{
+  let watcherId: number = 0;
+  const resultEl: HTMLElement | null = document.getElementById('result')!;
+  const mapElement: HTMLElement | null = document.getElementById('map')!;
+
+  // 현재 위치 값 구하기
+  const currentPosition = (position: GeolocationPosition) => {
+    const { latitude, longitude } = position.coords;
+    resultEl!.textContent = `📍 현재 위치: 위도 ${latitude}, 경도 ${longitude}`;
+
+    const latlng = new google.maps.LatLng(latitude, longitude);
+    const options: google.maps.MapOptions = {
+      zoom: 15,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+    };
+
+    const map = new google.maps.Map(mapElement, options);
+
+    const marker = new google.maps.Marker({
+      map: map,
+      icon: 'marker.jpeg',
+      title: 'my position',
+      position: latlng,
+    });
+  };
+
+  // 에러 처리
+  const errorCallback = (error: GeolocationPositionError) => {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        resultEl.textContent = `❌ 현재 위치 정보에 대한 권한이 없습니다.: ${error.message}`;
+        break;
+      case error.POSITION_UNAVAILABLE:
+        resultEl.textContent = `❌ 현재 위치 정보를 구할 수 없습니다.: ${error.message}`;
+        break;
+      case error.TIMEOUT:
+        resultEl.textContent = `❌ 시간 제한을 초과했습니다.: ${error.message}`;
+        break;
+    }
+  };
+
+  // 위치
+  function MyPosition(): void {
+    if (!resultEl) {
+      console.error('결과를 출력할 요소가 없습니다.');
+      return;
+    }
+
+    if (!navigator.geolocation) {
+      resultEl.textContent = '이 브라우저에서는 위치 정보를 지원하지 않습니다!';
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(currentPosition, errorCallback);
+  }
+
+  // 위치 추적
+  function StartPos(): void {
+    if (navigator.geolocation) {
+      const options: PositionOptions = { timeout: 1000 };
+      watcherId = navigator.geolocation.watchPosition(
+        currentPosition,
+        errorCallback,
+        options
+      );
+    }
+  }
+}
